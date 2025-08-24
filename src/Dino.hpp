47b76c8fe8ae780940c708de899dfa20b36b7e4b
HUY <HUY@DESKTOP-BNBCUP2>
2025-08-24 23:52:34 +0700

    Draw menu and check collision dino

---
+++ src/Dino.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <raylib.h>
+
+class Dino
+{
+public:
+    Dino();
+    ~Dino();
+    void Draw();
+    void Update();
+    Rectangle GetRect();
+    void DrawHitbox(bool isColliding);
+
+private:
+    Texture2D image;
+    Vector2 position;
+    int speed;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,45 @@
 #include <raylib.h>
-#include "ball.h"
+#include <iostream>
+#include "Button.hpp"
+#include "Dino.hpp"
 
-int main() 
+int main()
 {
-    const Color darkGreen = {20, 160, 133, 255};
-    
-    constexpr int screenWidth = 800;
-    constexpr int screenHeight = 600;
-    
-    Ball ball;
-    
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    InitWindow(1200, 800, "Raylib Buttons Tutorial");
+
     SetTargetFPS(60);
-    
-    while (!WindowShouldClose())
+
+    Texture2D background = LoadTexture("Graphics/background.png");
+    Button startButton{"Graphics/start_button.png", {300, 150}, 0.65};
+    Button exitButton{"Graphics/exit_button.png", {300, 300}, 0.65};
+    Dino dino;
+    Rectangle obstacle = Rectangle{800, 200, 200, 175};
+    bool exit = false;
+
+    while (WindowShouldClose() == false && exit == false)
     {
-        ball.Update();
-        
+        // Vector2 mousePosition = GetMousePosition();
+        // bool mousePressed = IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
+
+        // if (startButton.isPressed(mousePosition, mousePressed))
+        // {
+        //     std::cout << "Start Button Pressed" << std::endl;
+        // }
+
+        // if (exitButton.isPressed(mousePosition, mousePressed))
+        // {
+        //     exit = true;
+        // }
+        dino.Update();
+        bool isColliding = CheckCollisionRecs(dino.GetRect(), obstacle);
         BeginDrawing();
-            ClearBackground(darkGreen);
-            ball.Draw();
+        ClearBackground(WHITE);
+        // DrawTexture(background, 0, 0, WHITE);
+        //  startButton.Draw();
+        //  exitButton.Draw();
+        DrawRectangleLinesEx(obstacle, 5, BLACK);
+        dino.Draw();
+        dino.DrawHitbox(isColliding);
         EndDrawing();
     }
-    
-    CloseWindow();
 }
